910c1ab6e5dd336f7a1db4fdee0d350cdc5516d0
mukund <mukundt513@gmail.com>
2025-07-15 02:25:13 +0530

    relayer-ws connect with every websocket servers and send send to every websocket connectedwith it

---
+++ relayer-ws/index.ts
@@ -0,0 +1,20 @@
+import  { WebSocketServer,WebSocket } from "ws";
+
+const wss = new WebSocketServer({port: 3001});
+
+const servers : WebSocket[] = [];
+
+  
+
+
+wss.on("connection",function connection(ws) {
+   ws.on('error', console.error);
+   servers.push(ws);
+
+    ws.on("message",(data:string) =>{
+      servers.map(socket => {
+        socket.send(data);
+      })
+    })
+
+})
